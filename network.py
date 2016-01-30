f2bd2d2cfcc8b4857e2f12cb3233ba2f7c3c36a8
ciechowoj <keepitsimplesirius@gmail.com>
2016-01-30 12:03:12 +0100

    Added json support.

---
+++ network.py
@@ -0,0 +1,132 @@
+from __future__ import print_function
+
+import theano
+import theano.tensor.signal.downsample
+from utils import *
+import time
+import pickle
+import json
+import sys
+
+from layers import *
+from network import *
+from train import *
+
+class Network:
+	def snapshot(self):
+		params = [p.get_value(borrow = False).tolist() for p in self.params]
+		velocities = [v.get_value(borrow = False).tolist() for v in self.velocities]
+		variables = [v.get_value(borrow = False).tolist() for v in self.variables]
+
+		result = {
+			"params" : params,
+			"velocities" : velocities,
+			"variables" : variables
+		}
+
+		return result
+
+	def _load(self, source):
+		with open(source, 'rb') as file:
+			try:
+				snapshot, records = pickle.load(file)
+				self.load(snapshot, False)
+				return records
+			except KeyError:
+				pass
+
+			file.seek(0)
+			data = json.load(file)
+			self.load(data["snapshot"], False)
+
+			return data["record"]
+
+	def load(self, source, file = True):
+		if file and isinstance(source, str):
+			return self._load(source)
+		else:
+			for p, s in zip(self.params, source["params"]):
+				p.set_value(s, borrow = False)
+
+			for v, s in zip(self.velocities, source["velocities"]):
+				v.set_value(s, borrow = False)
+
+			for v, s in zip(self.variables, source["variables"]):
+				v.set_value(s, borrow = False)
+
+	def dump(self, path, records = None):
+		with open(path, 'wb+') as file:
+			json.dump({ "snapshot" : self.snapshot(), "record" : records }, file, sort_keys=True, indent=4, separators=(',', ': '))
+
+def compile(template):
+	X = theano.tensor.tensor4('X', dtype = 'float32')
+	Y = theano.tensor.matrix('Y', dtype = 'uint8')
+	
+	model_parameters = template.params
+
+	test = theano.tensor.scalar('test', dtype = 'float32')
+
+	log_probs = template(X, test)
+
+	predictions = theano.tensor.argmax(log_probs, axis = 1)
+
+	error_rate = theano.tensor.neq(predictions,Y.ravel()).mean()
+	nll = - theano.tensor.log(log_probs[theano.tensor.arange(Y.shape[0]), Y.ravel()]).mean()
+
+	weight_decay = 0.0
+	for p in model_parameters:
+		if p.name.endswith('weights'):
+			weight_decay = weight_decay + 1e-3 * (p ** 2).sum()
+
+	cost = nll + weight_decay
+	
+	learn_rate = theano.tensor.scalar('learn_rate', dtype = 'float32')
+	momentum = theano.tensor.scalar('momentum', dtype = 'float32')
+
+	# Theano will compute the gradients for us
+	gradients = theano.grad(cost, model_parameters)
+
+	#initialize storage for momentum
+	velocities = [theano.shared(numpy.zeros_like(p.get_value()), name='V_%s' %(p.name, )) for p in model_parameters]
+	
+	updates = []
+
+	for p, g, v in zip(model_parameters, gradients, velocities):
+		v_new = momentum * v - learn_rate * g
+		p_new = p + v_new
+		updates += [(v, v_new), (p, p_new)]
+	
+	def init_parameters():
+		rng = numpy.random.RandomState(1234)
+		
+		for p in model_parameters:
+			p.set_value(p.tag.initializer.generate(rng, p.get_value().shape))
+			
+		for v in velocities:
+			v.set_value(numpy.zeros_like(v.get_value()))
+			
+	step = theano.function(
+		[X, Y, learn_rate, momentum, test],
+		[cost, error_rate, nll, weight_decay],
+		updates = updates, 
+		allow_input_downcast = True,
+		on_unused_input = 'warn')
+			
+	predict = theano.function([X, test], predictions)
+	
+	init_parameters()
+	
+	def step_ex(X, Y, learn_rate, momentum):
+		return step(X, Y, learn_rate, momentum, 0)
+
+	def predict_ex(X):
+		return predict(X, 1)
+
+	network = Network()
+	network.step = step_ex
+	network.predict = predict_ex
+	network.params = model_parameters
+	network.velocities = velocities
+	network.variables = template.variables
+
+	return network
---
+++ train.py
@@ -0,0 +1,103 @@
+
+from __future__ import print_function
+
+import theano
+import theano.tensor.signal.downsample
+from utils import *
+import time
+import pickle
+import sys
+
+from layers import *
+from network import *
+from train import *
+
+def train(network, test_stream, validation_stream, learn_rate0, momentum):
+	batch = 0
+	epoch = 0
+	epoch_offset = 0
+
+	train_erros = []
+	train_loss = []
+	train_nll = []
+	validation_errors = []
+
+	number_of_epochs = 3
+	patience_expansion = 2
+
+	print_hline()
+	print_header()
+	print_hline()
+
+	quit = False
+
+	records = []
+	if len(sys.argv) == 2:
+		records = network.load(sys.argv[1])
+
+	for record in records:
+		print_record(record)
+
+	try:
+		epoch, number_of_epochs, batch = [int(s.strip()) for s in records[-1][0].split('/')]
+		epoch_offset = batch
+	except:
+		epoch, number_of_epochs, batch = (0, 3, 0)
+
+	best_valid_error_rate = numpy.inf
+	best_params = network.snapshot()
+	best_params_epoch = 0
+
+	# training loop
+	try:
+		start = time.time()
+		
+		while not quit and epoch < number_of_epochs: #This loop goes over epochs
+			epoch += 1
+			#First train on all data from this batch
+
+			epoch_start_batch = batch
+
+			for X_batch, Y_batch in test_stream.get_epoch_iterator(): 
+				batch += 1
+
+				K = 100000
+				learn_rate = learn_rate0 * K / numpy.maximum(K, batch)
+				
+				L, err_rate, nll, wdec = network.step(X_batch, Y_batch, learn_rate, momentum)
+
+				train_loss.append((batch, L))
+				train_erros.append((batch, err_rate))
+				train_nll.append((batch, nll))
+
+			# After an epoch compute validation error
+			val_error_rate = compute_error_rate(validation_stream, network.predict)
+			if val_error_rate < best_valid_error_rate:
+				number_of_epochs = numpy.maximum(number_of_epochs, int(epoch * patience_expansion + 1))
+				best_valid_error_rate = val_error_rate
+				best_params = network.snapshot()
+				best_params_epoch = epoch
+			validation_errors.append((batch, val_error_rate))
+
+			record = (
+				"{} / {} / {}".format(epoch, number_of_epochs, batch), 
+				val_error_rate * 100, 
+				numpy.mean(numpy.asarray(train_erros)[epoch_start_batch - epoch_offset:, 1]) * 100, 
+				numpy.mean(numpy.asarray(train_nll)[epoch_start_batch - epoch_offset:, 1]),
+				numpy.mean(numpy.asarray(train_loss)[epoch_start_batch - epoch_offset:, 1]))
+
+			records.append(record)
+			print_record(record)
+			
+		if (epoch + 1) % 25 and len(sys.argv) == 2:
+			network.dump("{}.{}.bn".format(sys.argv[1], time.strftime("%d_%b_%Y_%H_%M_%S")), records)
+
+	except KeyboardInterrupt:
+		pass
+	except:
+		network.dump("{}.nn".format(time.strftime("%d_%b_%Y_%H_%M_%S")), records)
+		raise
+
+	print("Setting network parameters from after epoch %d" %(best_params_epoch))
+	network.load(best_params)
+	network.dump("{}.nn".format(time.strftime("%d_%b_%Y_%H_%M_%S")), records)
