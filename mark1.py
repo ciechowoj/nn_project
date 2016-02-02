8ad667564a34b67a812adf039067808ce5b2138a
ciechowoj <keepitsimplesirius@gmail.com>
2016-02-02 20:59:23 +0100

    Disable data augumentation.

---
+++ mark1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+
+from main import run
+from layers import *
+
+nn = compose(
+    conv2D(3, 64, 5), 
+    relu(), 
+    max_pool_2d(2),
+    conv2D(64, 128, 5), 
+    relu(), 
+    max_pool_2d(2),
+    flatten(),
+    xaffine(3200, 625),
+    relu(),
+    xaffine(625, 10),
+    relu(),
+    softmax()
+    )
+
+run(network, "mark1")
