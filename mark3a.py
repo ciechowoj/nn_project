998f59be3748742bfd100082258fc0c9f3480c6f
ciechowoj <keepitsimplesirius@gmail.com>
2016-02-03 17:30:38 +0100

    Adds new test case.

---
+++ mark3a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+
+from main import run
+from layers import *
+
+network = compose(
+    conv2D(3, 128, 5), 
+    relu(), 
+    max_pool_2d(2),
+    conv2D(128, 128, 5),
+    relu(), 
+    max_pool_2d(2),
+    flatten(),
+    xaffine(3200, 625),
+    relu(),
+    xaffine(625, 625),
+    relu(),
+    xaffine(625, 10),
+    relu(),
+    softmax()
+    )
+
+run(network, "mark3a")
---
+++ mark3b.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+
+from main import run
+from layers import *
+
+network = compose(
+    conv2D(3, 128, 5), 
+    relu(), 
+    max_pool_2d(2),
+    conv2D(128, 128, 5),
+    relu(), 
+    max_pool_2d(2),
+    flatten(),
+    xaffine(3200, 625),
+    bnorm(625, 0.1),
+    relu(),
+    xaffine(625, 625),
+    bnorm(625, 0.1),
+    relu(),
+    xaffine(625, 10),
+    relu(),
+    softmax()
+    )
+
+run(network, "mark3b")
